2f3fa3194426d643a338638710e9d5bc595514aa
Rafael Antonio Mena Lopez <rafamenalopez@gmail.com>
2021-02-19 20:56:19 -0700

    first commit

---
+++ src/main/java/com/rmena/contactlistdashboardapp/ContactListDashboardAppApplication.java
@@ -0,0 +1,13 @@
+package com.rmena.contactlistdashboardapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactListDashboardAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactListDashboardAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmena/contactlistdashboardapp/ContactListDashboardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmena.contactlistdashboardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactListDashboardAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
